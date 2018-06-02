1d0d397e2d157c35d02ef39ad7d47bdee68d23d3
lzj112 <872575628@qq.com>
2018-06-02 19:35:48 +0800

    epoll反应堆模型

--- libevent/epoll_reactor.c
+++ libevent/epoll_reactor.c
@@ -18,30 +18,49 @@
  * status:1表示在监听事件中，0表示不在 
  * last_active:记录最后一次响应时间,做超时处理
  */
-struct myevent_s {
-    int fd;                 //监听套接字
+struct myevent_s 
+{
+    int fd;                 //cfd listenfd
     int events;             //EPOLLIN  EPLLOUT
-    void *arg;              //指向自己的结构体指针
-    void (*call_back)(int fd, int events, void *arg);   //回调函数
-    int status;             //在不在红黑树中的标志
+    void *arg;              //指向自己结构体指针
+    void (*call_back)(int fd, int events, void *arg);
+    int status;
     char buf[BUFLEN];
     int len;
-    long last_active;       
+    long last_active;
 };
 
 int g_efd;          /* epoll_create返回的句柄 */
 struct myevent_s g_events[MAX_EVENTS+1];   /* +1 最后一个用于 listen fd */
 
-//初始化 myevent_s结构体
-void eventset(struct myevent_s *ev, int fd, void (*call_back)(int, int, void *), void *arg)
+/*
+struct epoll_event 
+结构体epoll_event被用于注册所感兴趣的事件和回传所发生待处理的事件，定义如下：
+
+struct epoll_event 
+{
+    __uint32_t events;       epoll event 
+    epoll_data_t data;       User data variable 
+};
+typedef union epoll_data 
+{
+    void *ptr;
+    int fd;
+    __uint32_t u32;
+    __uint64_t u64;
+} epoll_data_t; //保存触发事件的某个文件描述符相关的数据
+*/
+
+//初始化myevent_s类型
+void eventset(struct myevent_s* ev, int fd, void (*call_back)(int, int, void *), void* arg)
 {
     ev->fd = fd;
     ev->call_back = call_back;
     ev->events = 0;
     ev->arg = arg;
     ev->status = 0;
-    //memset(ev->buf, 0, sizeof(ev->buf));
-    //ev->len = 0;
+    // memset(ev->buf, 0, sizeof(ev->buf));  本模型将收到的数据再发回去，所以不memset
+    // ev->len = 0;
     ev->last_active = time(NULL);
 
     return;
@@ -50,6 +69,7 @@ void eventset(struct myevent_s *ev, int fd, void (*call_back)(int, int, void *),
 void recvdata(int fd, int events, void *arg);
 void senddata(int fd, int events, void *arg);
 
+//将fd添加到epoll注册的事件合集
 void eventadd(int efd, int events, struct myevent_s *ev)
 {
     struct epoll_event epv = {0, {0}};
@@ -75,12 +95,15 @@ void eventadd(int efd, int events, struct myevent_s *ev)
     return;
 }
 
+//从事件合集中删除
 void eventdel(int efd, struct myevent_s *ev)
 {
     struct epoll_event epv = {0, {0}};
 
-    if (ev->status != 1)    //不在红黑树里
-        return;
+    if (ev->status != 1)
+    {
+        return ;
+    }
 
     epv.data.ptr = ev;
     ev->status = 0;
@@ -90,8 +113,10 @@ void eventdel(int efd, struct myevent_s *ev)
 }
 
 
+//当有新的连接时，调用监听listenfd的此回调函数，指定新sockfd的回调函数
 void acceptconn(int lfd, int events, void *arg)
 {
+    printf("****************************\n");
     struct sockaddr_in cin;
     socklen_t len = sizeof(cin);
     int cfd, i;
@@ -107,20 +132,22 @@ void acceptconn(int lfd, int events, void *arg)
     }
 
     do {
-        for (i = 0; i < MAX_EVENTS; i++) //遍历g_events数组
+        for (i = 0; i < MAX_EVENTS; i++) 
         {
-            if (g_events[i].status == 0)    //找到一个空闲元素
+            if (g_events[i].status == 0)
+            {
                 break;
+            }
         }
 
-        if (i == MAX_EVENTS) //无空闲元素
+        if (i == MAX_EVENTS) 
         {
             printf("%s: max connect limit[%d]\n", __func__, MAX_EVENTS);
             break;
         }
 
         int flag = 0;
-        if ((flag = fcntl(cfd, F_SETFL, O_NONBLOCK)) < 0)   //设置cfd为非阻塞
+        if ((flag = fcntl(cfd, F_SETFL, O_NONBLOCK)) < 0)
         {
             printf("%s: fcntl nonblocking failed, %s\n", __func__, strerror(errno));
             break;
@@ -141,22 +168,25 @@ void recvdata(int fd, int events, void *arg)
     int len;
 
     len = recv(fd, ev->buf, sizeof(ev->buf), 0);
-    eventdel(g_efd, ev);
+    eventdel(g_efd, ev);    //接受完数据 删除该监听事件
 
-    if (len > 0) {
+    if (len > 0) 
+    {
         ev->len = len;
         ev->buf[len] = '\0';
         printf("C[%d]:%s\n", fd, ev->buf);
         /* 转换为发送事件 */
         eventset(ev, fd, senddata, ev);
-        eventadd(g_efd, EPOLLOUT, ev);
+        eventadd(g_efd, EPOLLOUT, ev); //再把其添加到监听事件中,此时回调函数改为了senddata
     }
-    else if (len == 0) {
+    else if (len == 0) 
+    {
         close(ev->fd);
         /* ev-g_events 地址相减得到偏移元素位置 */
         printf("[fd=%d] pos[%d], closed\n", fd, (int)(ev - g_events));
     }
-    else {
+    else 
+    {
         close(ev->fd);
         printf("recv[fd=%d] error[%d]:%s\n", fd, errno, strerror(errno));
     }
@@ -170,16 +200,18 @@ void senddata(int fd, int events, void *arg)
     int len;
 
     len = send(fd, ev->buf, ev->len, 0);
-    //printf("fd=%d\tev->buf=%s\ttev->len=%d\n", fd, ev->buf, ev->len);
-    //printf("send len = %d\n", len);
+    printf("fd=%d\tev->buf=%s\ttev->len=%d\n", fd, ev->buf, ev->len);
+    printf("send len = %d\n", len);
 
-    eventdel(g_efd, ev);
-    if (len > 0) {
+    eventdel(g_efd, ev); //删除该事件
+    if (len > 0) 
+    {
         printf("send[fd=%d], [%d]%s\n", fd, len, ev->buf);
         eventset(ev, fd, recvdata, ev);
-        eventadd(g_efd, EPOLLIN, ev);
+        eventadd(g_efd, EPOLLIN, ev);   //添加该事件，已更改回调函数为recvdata
     }
-    else {
+    else 
+    {
         close(ev->fd);
         printf("send[fd=%d] error %s\n", fd, strerror(errno));
     }
@@ -187,6 +219,7 @@ void senddata(int fd, int events, void *arg)
     return;
 }
 
+//初始化一个sockfd
 void initlistensocket(int efd, short port)
 {
     int lfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -224,19 +257,25 @@ int main(int argc, char *argv[])
 
     /* 事件循环 */
     struct epoll_event events[MAX_EVENTS+1];
-
     printf("server running:port[%d]\n", port);
     int checkpos = 0, i;
-    while (1) {
+    while (1) 
+    {
         /* 超时验证，每次测试100个链接，不测试listenfd 当客户端60秒内没有和服务器通信，则关闭此客户端链接 */
         long now = time(NULL);
-        for (i = 0; i < 100; i++, checkpos++) {
+        for (i = 0; i < 100; i++, checkpos++) 
+        {
             if (checkpos == MAX_EVENTS)
+            {
                 checkpos = 0;
+            }
             if (g_events[checkpos].status != 1)
+            {
                 continue;
+            }
             long duration = now - g_events[checkpos].last_active;
-            if (duration >= 60) {
+            if (duration >= 60) 
+            {
                 close(g_events[checkpos].fd);
                 printf("[fd=%d] timeout\n", g_events[checkpos].fd);
                 eventdel(g_efd, &g_events[checkpos]);
@@ -244,16 +283,23 @@ int main(int argc, char *argv[])
         }
         /* 等待事件发生 */
         int nfd = epoll_wait(g_efd, events, MAX_EVENTS+1, 1000);
-        if (nfd < 0) {
+        if (nfd < 0) 
+        {
             printf("epoll_wait error, exit\n");
             break;
         }
-        for (i = 0; i < nfd; i++) {
+
+        for (i = 0; i < nfd; i++) 
+        {
             struct myevent_s *ev = (struct myevent_s *)events[i].data.ptr;
-            if ((events[i].events & EPOLLIN) && (ev->events & EPOLLIN)) {
+            if ((events[i].events & EPOLLIN) && (ev->events & EPOLLIN)) 
+            {
+                //可读
                 ev->call_back(ev->fd, events[i].events, ev->arg);
             }
-            if ((events[i].events & EPOLLOUT) && (ev->events & EPOLLOUT)) {
+            if ((events[i].events & EPOLLOUT) && (ev->events & EPOLLOUT)) 
+            {
+                //可写
                 ev->call_back(ev->fd, events[i].events, ev->arg);
             }
         }
